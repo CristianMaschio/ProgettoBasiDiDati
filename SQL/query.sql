5d5d8be07d0d2c0ae93f6282baaa9c140d74619a
CristianMaschio <blackspartan@hotmail.it>
2017-09-11 09:39:50 +0200

    Progetto Finito

--- SQL/query.sql
+++ SQL/query.sql
@@ -13,11 +13,11 @@ FROM (docente LEFT JOIN insegnamento ON docente.Matricola=insegnamento.IdDocente
 -- -------------------------------------------------------- 
 --
 -- Mostra tutte le aule prenotate da ciascun corso in ordine di corso
--- OUTPUT: Corso | Aula | Data inizo | Data fine
+-- OUTPUT: Corso | Aula | DataInizo | DataFine
 --
 
 
-SELECT corso.Nome AS Corso, prenotazione.IdStanza AS Aula, prenotazione.DataInizio AS Data inzio, prenotazione.DataFine AS Data fine
+SELECT corso.Nome AS Corso, prenotazione.IdStanza AS Aula, prenotazione.DataInizio AS DataInzio, prenotazione.DataFine AS DataFine
 FROM prenotazione LEFT JOIN corso ON prenotazione.IdCorso = corso.Codice
 ORDER BY Corso
 
@@ -26,9 +26,10 @@ ORDER BY Corso
 -- Mostra tutte le aule prenotate in un certo lasso ti tempo
 -- OUTPUT: Stanza | Corso | Data inizo | Data fine
 --
-SELECT IdStanza AS Stanza, corso.Nome AS Corso, DataInizio AS Data inizio, DataFine AS Data fine
+SELECT IdStanza AS Stanza, corso.Nome AS Corso, DataInizio AS DataInizio, DataFine AS DataFine
 FROM prenotazione LEFT JOIN corso ON prenotazione.IdCorso = corso.Codice
-WHERE DataInizio >= '2017-07-20 00:00:00' AND DataFine <= '2017-07-22 00:00:00'
+WHERE (DataInizio >= '2017-10-20 00:00:00' AND DataInizio <= '2017-10-20 12:00:00') OR 
+        (DataFine >= '2017-10-20 00:00:00' AND DataFine <= '2017-10-20 12:00:00')
 
 -- -------------------------------------------------------- 
 --
@@ -40,4 +41,22 @@ FROM stanza
 WHERE Nome NOT IN(
     SELECT IdStanza AS Nome
     FROM prenotazione LEFT JOIN corso ON prenotazione.IdCorso = corso.Codice
-    WHERE DataInizio >= '2017-07-20 00:00:00' AND DataFine <= '2017-07-22 00:00:00')
+    WHERE (DataInizio >= '2017-10-20 00:00:00' AND DataInizio <= '2017-10-20 12:00:00') OR 
+            (DataFine >= '2017-10-20 00:00:00' AND DataFine <= '2017-10-20 12:00:00'))
+
+
+-- -------------------------------------------------------- 
+--
+-- Per ogni insegnamento, mostra, docente, supporto, con ulteriori informazioni su docente e supporto, in ordine di Corso
+-- OUTPUT: Corso, DataInizio, DataFine, NomeDocente, CognomeDocente, CategoriaDocente, UfficioDocente, 
+-- NomeSupporto, CognomeSupporto, CategoriaSupporto, UfficioSupporto
+--   
+SELECT corso.Nome AS Corso, insegnamento.DataInizio, insegnamento.DataFine, d.Nome AS NomeDocente, 
+        d.Cognome AS CognomeDocente, d.Categoria AS CategoriaDocente, d.IdStanza AS UfficioDocente, 
+        docente.Nome AS NomeSupporto, docente.Cognome AS CognomeSupporto, 
+        docente.Categoria AS CategoriaSupporto, docente.IdStanza AS UfficioSupporto
+FROM ((insegnamento LEFT JOIN supporto ON insegnamento.IdInsegnamento=supporto.IdInsegnamento) 
+        LEFT JOIN docente ON supporto.IdDocenteSupporto=docente.Matricola) 
+        LEFT JOIN corso ON corso.Codice=insegnamento.IdCorso 
+        LEFT JOIN docente AS d ON d.Matricola=insegnamento.IdDocente 
+ORDER BY corso.Nome
